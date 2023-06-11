bb0fa2efbffa2bf1999a4a51d414371358f77b51
khisislam18 <khisislam18@gmail.com>
2023-06-11 15:44:02 +0300

    add 1st homework

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,47 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int ind = 0;
 
     void clear() {
+        storage = new Resume[10000];
+        ind = 0;
     }
 
     void save(Resume r) {
+        storage[ind++] = r;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < ind; i++) {
+            if(uuid.equals(storage[i].uuid)){
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < ind; i++) {
+            if(uuid.equals(storage[i].uuid)){
+                Resume tmp = storage[i];
+                storage[i] = storage[ind - 1];
+                storage[ind - 1] = tmp;
+                ind--;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] tmpRes = new Resume[ind];
+        System.arraycopy(storage, 0,tmpRes,0,ind);
+        return tmpRes;
     }
 
     int size() {
-        return 0;
+        return ind;
     }
 }
